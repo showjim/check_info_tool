59dc7fc52dabfe798ae2c3026d301a9eaace910f
Chao Zhou <zhouchao486@gmail.com>
2024-05-08 13:41:22 +0800

    add link

--- check_info_webapp.py
+++ check_info_webapp.py
@@ -57,7 +57,7 @@ def main(app=Application):
     # Sidebar for menu options
     with st.sidebar:
         st.header("Other Tools")
-        st.page_link("http://taishanstone:8501", label="Pattern Auto Edit Tool", icon="1️⃣")
+        st.page_link("http://taishanstone:8502", label="Pattern Auto Edit Tool", icon="1️⃣")
         st.page_link("http://taishanstone:8503", label="Shmoo Detect Tool", icon="2️⃣")
         st.header("Help")
         if st.button("About"):
