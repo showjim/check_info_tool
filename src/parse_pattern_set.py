7cf754f2f6ef43014d86c226f968ba89098f3e32
chao zhou <zhouchao486@gmail.com>
2024-05-08 21:54:48 +0800

    optimise patset parse

--- src/parse_pattern_set.py
+++ src/parse_pattern_set.py
@@ -120,17 +120,33 @@ class ParsePatternSet:
     def get_pattern_cycle_info(self):
         return self.__cycle_dict
 
+    # def get_pattern_set_info(self):
+    #     result_dict = {}
+    #     result_dict = copy.deepcopy(self.__pattern_set_dict)
+    #     for patset, pat_list in self.__pattern_set_dict.items():
+    #         for i, pat in enumerate(pat_list):
+    #             if (not pat.upper().endswith("PATX")) and (not pat.upper().endswith("PAT")):
+    #                 if pat.upper() in result_dict.keys():
+    #                     result_dict[patset][i] = result_dict[pat.upper()]
+    #                 else:
+    #                     print("Error: Patset found item without pattern definition: " + pat)
+    #     return result_dict #self.__pattern_set_dict
+
     def get_pattern_set_info(self):
         result_dict = {}
-        result_dict = copy.deepcopy(self.__pattern_set_dict)
         for patset, pat_list in self.__pattern_set_dict.items():
-            for i, pat in enumerate(pat_list):
-                if (not pat.upper().endswith("PATX")) and (not pat.upper().endswith("PAT")):
-                    if pat.upper() in result_dict.keys():
-                        result_dict[patset][i] = result_dict[pat.upper()]
+            result_dict[patset] = []
+            for pat in pat_list:
+                upper_pat = pat.upper()
+                if not (upper_pat.endswith("PATX") or upper_pat.endswith("PAT")):
+                    if upper_pat in self.__pattern_set_dict:
+                        result_dict[patset].append(self.__pattern_set_dict[upper_pat])
                     else:
-                        print("Error: Patset found item without pattern definition: " + pat)
-        return result_dict #self.__pattern_set_dict
+                        # 这里可以抛出异常或者记录日志
+                        raise ValueError(f"Error: Patset '{patset}' found item without pattern definition: {pat}")
+                else:
+                    result_dict[patset].append(pat)  # 保持原始值
+        return result_dict
 
     def get_pattern_tset(self):
         return self.__tset_name
